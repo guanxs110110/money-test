7523143702ee1cebaa8d77c5014688200cb30b83
guanxs110110 <471174550@qq.com>
2024-12-10 22:03:23 +0800

    Add files via upload

--- src/main/java/com/guan/money/service/BankAccountService.java
+++ src/main/java/com/guan/money/service/BankAccountService.java
@@ -1,35 +1,60 @@
 package com.guan.money.service;
 
 import java.math.BigDecimal;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 import javax.money.MonetaryAmount;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 import org.javamoney.moneta.Money;
+import org.junit.Assert;
 import org.springframework.stereotype.Service;
 
 import com.guan.money.entity.BankAccount;
 
 @Service
 public class BankAccountService {
-	
-	private EntityManagerFactory entityManagerFactory;
-    private EntityManager entityManager;
+
+    private EntityManagerFactory entityManagerFactory;
+
+    private ThreadLocal<EntityManager> entityManagerThreadLocal = new ThreadLocal<EntityManager>();
 
     public BankAccountService() {
-    	
+
         this.entityManagerFactory = Persistence.createEntityManagerFactory("BankAccount-unit");
-        this.entityManager = entityManagerFactory.createEntityManager();
-        
+
         //init TestAcountA&TestAcountB in H2 database
         createBankAccount("TestAcountA", "HKD", new BigDecimal(100000));
         createBankAccount("TestAcountB", "HKD", new BigDecimal(100000));
     }
+
+    /**
+     * get thread local entityManager to avoid thread safe issue
+     * @return
+     */
+    private synchronized EntityManager getEntityManager() {
+        EntityManager entityManager = entityManagerThreadLocal.get();
+        if (entityManager == null) {
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManagerThreadLocal.set(entityManager);
+        }
+
+        return  entityManager;
+    }
+
+    public synchronized void closeEntityManager() {
+        EntityManager entityManager = entityManagerThreadLocal.get();
+        if (entityManager != null) {
+            entityManager.clear();
+            entityManager.close();
+            entityManagerThreadLocal.set(null);
+        }
+    }
+
 
     /**
      * Create a new bank account
@@ -38,6 +63,7 @@ public class BankAccountService {
      * @param balance
      */
     public void createBankAccount(String name, String currency, BigDecimal balance) {
+        EntityManager entityManager = getEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         try {
             transaction.begin(); // Start transaction
@@ -51,17 +77,36 @@ public class BankAccountService {
             transaction.rollback(); // Rollback on error
             throw e;
         }
-        
+
     }
-    
+
     /**
      * retrieve account by account name and lock the account row in db
      * @param accountName
      * @return
      */
     public BankAccount getBankAcount(String accountName) {
+        EntityManager entityManager = getEntityManager();
         return entityManager.createQuery("SELECT e FROM BankAccount e where e.name = '" + accountName + "'", BankAccount.class)
-                .getSingleResult();
+                .getResultList().get(0);
+    }
+
+    /**
+     * update bank account balance by Optimistic Lock
+     * @param bankAccount
+     */
+    public void updateBankAccountBalanceWithOptimisticLock(BankAccount bankAccount) {
+        EntityManager entityManager = getEntityManager();
+
+        String sql = "update BankAccount e set e.balance = " + bankAccount.getBalance() + ", e.version = e.version + 1 where e.name = '"
+                + bankAccount.getName() + "' and e.version = " + bankAccount.getVersion();
+        Query query = entityManager.createNativeQuery(sql, BankAccount.class);
+
+        int updateRows = query.executeUpdate();
+
+        if (updateRows == 0) {
+            throw new RuntimeException("Account data has been changed, please retry again.");
+        }
     }
 
     /**
@@ -72,49 +117,51 @@ public class BankAccountService {
      * @param currency
      */
     public void transferAccount(String fromAccountName, String toAccountName, BigDecimal amount, String currency) {
+        EntityManager entityManager = getEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
+
         try {
             transaction.begin();
-            
+
             //load the accounts for update
             BankAccount fromAccount = getBankAcount(fromAccountName);
             BankAccount toAccount = getBankAcount(toAccountName);
-            
+
             if (fromAccount == null || toAccount == null) {
-            	throw new RuntimeException("bank account can not be null!");
+                throw new RuntimeException("bank account can not be null!");
             }
-            
+
             //check from account balance if it is sufficient or not
             checkAccountBalance(fromAccount, amount, currency);
-            
-        	CurrencyUnit currencyUnit = Monetary.getCurrency(currency);
-        	MonetaryAmount fromAccountAmount = Money.of(fromAccount.getBalance(), currencyUnit);
-        	MonetaryAmount toAccountAmount = Money.of(toAccount.getBalance(), currencyUnit);
-        	
-        	MonetaryAmount toBeTransferedAmount = Money.of(amount, currencyUnit);
-        	
+
+            CurrencyUnit currencyUnit = Monetary.getCurrency(currency);
+            MonetaryAmount fromAccountAmount = Money.of(fromAccount.getBalance(), currencyUnit);
+            MonetaryAmount toAccountAmount = Money.of(toAccount.getBalance(), currencyUnit);
+
+            MonetaryAmount toBeTransferedAmount = Money.of(amount, currencyUnit);
+
             //transfer from account, and subtract the transferred amount
-        	MonetaryAmount fromAccountAmountAfterTransfer =  fromAccountAmount.subtract(toBeTransferedAmount);
-        	//update the from account balance
-        	fromAccount.setBalance(fromAccountAmountAfterTransfer.getNumber().numberValue(BigDecimal.class));
-        	
+            MonetaryAmount fromAccountAmountAfterTransfer =  fromAccountAmount.subtract(toBeTransferedAmount);
+            //update the from account balance
+            fromAccount.setBalance(fromAccountAmountAfterTransfer.getNumber().numberValue(BigDecimal.class));
+
             //add the transferred amount to the to account
-        	MonetaryAmount toAccountAmountAfterTransfer = toAccountAmount.add(toBeTransferedAmount);
-        	//update the to account balance
-        	toAccount.setBalance(toAccountAmountAfterTransfer.getNumber().numberValue(BigDecimal.class));
-        	
-        	//persist from and to accounts to db
-        	entityManager.persist(fromAccount);
-        	entityManager.persist(toAccount);
-        	
+            MonetaryAmount toAccountAmountAfterTransfer = toAccountAmount.add(toBeTransferedAmount);
+            //update the to account balance
+            toAccount.setBalance(toAccountAmountAfterTransfer.getNumber().numberValue(BigDecimal.class));
+
+            //persist from and to accounts to db
+            updateBankAccountBalanceWithOptimisticLock(fromAccount);
+            updateBankAccountBalanceWithOptimisticLock(toAccount);
+
             transaction.commit();
         } catch (Exception e) {
-        	//catch any exception, then roll back
+            //catch any exception, then roll back
             transaction.rollback();
             throw e;
-        } 
+        }
     }
-    
+
     /**
      * check account balance
      * @param acount
@@ -122,21 +169,21 @@ public class BankAccountService {
      * @param currency
      */
     public void checkAccountBalance(BankAccount acount, BigDecimal amount, String currency) {
-    	
-    	if (acount == null) {
-    		throw new RuntimeException("bank account can not be null!");
-    	}
+
+        if (acount == null) {
+            throw new RuntimeException("bank account can not be null!");
+        }
         if (amount ==null || amount.compareTo(BigDecimal.ZERO) <= 0) {
             throw  new RuntimeException("Transferred amount must be greater than zero!");
         }
-    	CurrencyUnit currencyUnit = Monetary.getCurrency(currency);
-    	MonetaryAmount accountAmount = Money.of(acount.getBalance(), currencyUnit);
-    	
-    	MonetaryAmount toBeTransferedAmount = Money.of(amount, currencyUnit);
-    	
-    	if (accountAmount.isLessThan(toBeTransferedAmount)) {
-    		throw new RuntimeException("bank account" + acount.getName() +  " doesn't has sufficient balance to transfer.");
-    	}
+        CurrencyUnit currencyUnit = Monetary.getCurrency(currency);
+        MonetaryAmount accountAmount = Money.of(acount.getBalance(), currencyUnit);
+
+        MonetaryAmount toBeTransferedAmount = Money.of(amount, currencyUnit);
+
+        if (accountAmount.isLessThan(toBeTransferedAmount)) {
+            throw new RuntimeException("bank account" + acount.getName() +  " doesn't has sufficient balance to transfer.");
+        }
     }
 
 }
